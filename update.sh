e088064feaba71bac220a59a17b10b9e628ce0e8
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2022-01-10 09:18:57 -0800

    add env-file

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ git pull
 docker rm newhartian
 docker build -t newhartian .
 docker rm newhartian
-docker run -d --name newhartian newhartian
+docker run -d --name newhartian --env-file .env newhartian
