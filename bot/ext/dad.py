dbe6bc6c758334e5340caf0085f41697968cc39f
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2022-01-10 18:45:38 -0800

    fix unicode

--- bot/ext/dad.py
+++ bot/ext/dad.py
@@ -7,7 +7,7 @@ from nextcord.ext import commands
 END_OF_SENTENCE_CHARS = [".", "!", "?", ",", ";", ":"]
 
 
-class Dad(commands.Cog):
+class DadCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
@@ -15,17 +15,17 @@ class Dad(commands.Cog):
     async def on_message(self, message: nextcord.Message) -> None:
         if message.author.bot or message.author.id == self.bot.user.id:
             return
-        content = unidecode(message.content.lower())
+        content = unidecode(message.content)
         location = None
         with suppress(ValueError):
-            location = message.content.lower().split(" ").index("im")
+            location = content.lower().split(" ").index("im")
         with suppress(ValueError):
-            location = message.content.lower().split(" ").index("i'm")
+            location = content.lower().split(" ").index("i'm")
         if location is None:
             return
         name = ""
-        for i in message.content.split(" ")[
-            location + 1 : min(location + 11, len(message.content.split(" ")))
+        for i in content.split(" ")[
+            location + 1 : min(location + 11, len(content.split(" ")))
         ]:
             if i[-1] in END_OF_SENTENCE_CHARS:
                 name += i[:-1] + " "
@@ -37,4 +37,4 @@ class Dad(commands.Cog):
         await message.channel.send("Hi {}, I'm dad!".format(name[:-1]))
 
 def setup(bot: commands.Bot) -> None:
-    bot.add_cog(Dad(bot))
+    bot.add_cog(DadCog(bot))
