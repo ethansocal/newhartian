c3cd054d101b9a7a8e95343d91172a277be49fae
Ethan <79533577+ethansocal@users.noreply.github.com>
2022-01-13 17:35:25 -0800

    Create internal.py

---
+++ bot/ext/internal.py
@@ -0,0 +1,17 @@
+from nextcord.ext import commands
+from aioconsole import aexec
+
+
+class InternalCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+    
+    @commands.command()
+    async def eval(self, ctx: commands.Context, *, code: str) -> None:
+        if code[:3] == "```" and code[-3:] == "```":
+            code = code[3:-3]
+            if code[:2] == "py":
+                code = code[2:]
+        code = code.strip()
+            
+        await ctx.send(await aexec(code, {"bot": self.bot, "here": ctx.channel, "message": ctx.message, "reply": ctx.channel.fetch_message(ctx.message.reference.message_id)}))
